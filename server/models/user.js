13aef12ac4a86c94eea8cfbd01029ad44770e525
Daxian Fang <daxianfang@gmail.com>
2017-07-20 22:21:09 -0700

    Disabled bcrypt due to deployment error

--- server/models/user.js
+++ server/models/user.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-var bcrypt = require('bcrypt');
+// var bcrypt = require('bcrypt');
 
 var UserSchema = new mongoose.Schema({
 	name: {
@@ -17,17 +17,17 @@ var UserSchema = new mongoose.Schema({
 	timestamps: true
 });
 
-UserSchema.methods.generateHash = function(password) {
-	return bcrypt.hashSync(password, bcrypt.genSaltSync(8))
-};
+// UserSchema.methods.generateHash = function(password) {
+// 	return bcrypt.hashSync(password, bcrypt.genSaltSync(8))
+// };
 
-UserSchema.methods.validPassword = function(password) {
-	return bcrypt.compareSync(password, this.password);	
-};
+// UserSchema.methods.validPassword = function(password) {
+// 	return bcrypt.compareSync(password, this.password);	
+// };
 
-UserSchema.pre('save', function(done) {
-	this.password = this.generateHash(this.password);
-	done();
-});
+// UserSchema.pre('save', function(done) {
+// 	this.password = this.generateHash(this.password);
+// 	done();
+// });
 
 mongoose.model('User', UserSchema);
